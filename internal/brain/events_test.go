187fcfb59e779b597202530fc340601699d4effa
ekelen <ekelen@student.42.us.org>
2019-06-23 15:14:17 +0200

    tests: Initialize unit tests
    unit test file for board
    
    unit test files for brain and player
    
    player unit test tables use instances of players

---
+++ internal/brain/events_test.go
@@ -0,0 +1,11 @@
+package brain
+
+import "testing"
+
+func TestStartRound(t *testing.T) {
+	t.Skip("Skipping, undecided about testing global state")
+}
+
+func TestHandleMove(t *testing.T) {
+	t.Skip("Skipping, undecided about testing global state")
+}

6a5529495d61e2d3d6a72c8b4ab2b5a340347583
eskombro <sjimenezre@gmail.com>
2019-07-28 17:58:02 +0200

    Random position, no crash

--- internal/brain/suggestor.go
+++ internal/brain/suggestor.go
@@ -8,33 +8,33 @@ import (
 
 func SuggestMove() {
 	possible := []int{}
-	best := []int{}
+	// best := []int{}
 	for i := 0; i < (board.SIZE * board.SIZE); i++ {
 		if GameRound.Goban.Tab[i] == 0 {
 			if CheckValidMove(i) {
-				seq := CompleteSequenceForPosition(i, GameRound.CurrentPlayer.Id)
-				if len(seq) > 0 {
-					best = append(best, i)
-				}
+				// seq := CompleteSequenceForPosition(i, GameRound.CurrentPlayer.Id)
+				// if len(seq) > 0 {
+				// 	best = append(best, i)
+				// }
 				possible = append(possible, i)
 			}
 		}
 	}
 	ran := 0
-	if len(best) > 0 {
-		if len(best)-1 < 2 {
-			ran = 0
-		} else {
-			ran = int(rand.Intn(len(best) - 1))
-		}
-		GameRound.SuggestedPosition = best[ran]
+	// if len(best) > 0 {
+	// 	if len(best)-1 < 2 {
+	// 		ran = 0
+	// 	} else {
+	// 		ran = int(rand.Intn(len(best) - 1))
+	// 	}
+	// 	GameRound.SuggestedPosition = best[ran]
+	// } else {
+	if len(possible)-1 < 2 {
+		ran = 0
 	} else {
-		if len(best)-1 < 2 {
-			ran = 0
-		} else {
-			ran = int(rand.Intn(len(best) - 1))
-		}
-		ran := int(rand.Intn(len(possible) - 1))
-		GameRound.SuggestedPosition = possible[ran]
+		ran = int(rand.Intn(len(possible) - 1))
 	}
+	ran = int(rand.Intn(len(possible) - 1))
+	GameRound.SuggestedPosition = possible[ran]
+	// }
 }

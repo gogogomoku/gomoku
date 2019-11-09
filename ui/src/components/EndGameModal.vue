abfec74594fb9b6ba2a5017a1037aa010ec4e365
Elizabeth Kelen <elizabethkelen@Elizabeths-MacBook-Pro.local>
2019-11-09 15:39:38 +0100

    add placeholder end game modal to replace popup

---
+++ ui/src/components/EndGameModal.vue
@@ -0,0 +1,139 @@
+<template>
+  <!-- https://vuejs.org/v2/examples/modal.html -->
+  <transition>
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <h3>lorem ipsum</h3>
+          </div>
+
+          <div class="modal-body">
+            <p>Some info</p>
+          </div>
+
+          <div class="modal-footer">
+            <button class="modal-default-button" @click="onClose()">OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+
+export default {
+  name: "EndGameModal",
+  props: {
+    showModal: {
+			type: Boolean,
+			required: true
+		},
+  },
+  methods: {
+    onClose() {
+      this.$parent.closeModal("EndGameModal");
+    },
+  }
+};
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #1c1d21;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #ece2d0;
+}
+
+.modal-body {
+  margin: 20px 0;
+  color: #bbb;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.modal-default-button {
+  float: right;
+  font-family: "Share Tech Mono", monospace;
+}
+
+.modal-footer {
+  padding-bottom: 28px;
+}
+
+/* button: same as StartButton.vue */
+button {
+  padding: 9px 12px 9px;
+  background-color: #3cadad;
+  border: none;
+  font-size: 16px;
+  font-weight: bolder;
+  letter-spacing: normal;
+  border-radius: 3px;
+}
+button:hover {
+  background-color: #48cece;
+  cursor: pointer;
+}
+button:active {
+  background-color: #2b7c7c;
+}
+
+.gameOptions {
+  text-align: right;
+}
+
+.gameOptions label {
+  margin-left: 3px;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
